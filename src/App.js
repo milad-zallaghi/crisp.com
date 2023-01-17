67585aa7a697270424dacd8a02dbe65e9ea585a8
Milad <milad.karizzma@gmail.com>
2023-01-17 12:57:32 +0330

    first push

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          test crisp <code></code>
+        </p>
+        <a
+          className="App-link"
+          href="crisp.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        test 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
